d1d21e8a8ec2d5fe0955eca35f03da06b00f06e6
Jason Watmore <cornflourblue@hotmail.com>
2017-02-18 12:06:59 +1100

    prevent view content loading on initial app load
    prevented angular from loading view content on initial load since it's
    pre rendered on the server

--- client/blog/app.js
+++ client/blog/app.js
@@ -7,6 +7,9 @@
         .run(run);
 
     function config($locationProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+        // set variable to control behaviour on initial app load
+        window.initialLoad = true;
+
         $locationProvider.html5Mode(true);
 
         // default route
@@ -16,31 +19,36 @@
             .state('home', {
                 url: '/?:page',
                 templateUrl: function (stateParams) {
-                    return '/?xhr=1' + (stateParams.page ? '&page=' + stateParams.page : '');
+                    return window.initialLoad ? null :
+                        '/?xhr=1' + (stateParams.page ? '&page=' + stateParams.page : '');
                 }
             })
             .state('post-details', {
                 url: '/post/:year/:month/:day/:slug',
                 templateUrl: function (stateParams) {
-                    return '/post/' + stateParams.year + '/' + stateParams.month + '/' + stateParams.day + '/' + stateParams.slug + '?xhr=1';
+                    return window.initialLoad ? null :
+                        '/post/' + stateParams.year + '/' + stateParams.month + '/' + stateParams.day + '/' + stateParams.slug + '?xhr=1';
                 }
             })
             .state('posts-for-tag', {
                 url: '/posts/tag/:tag',
                 templateUrl: function (stateParams) {
-                    return '/posts/tag/' + stateParams.tag + '?xhr=1';
+                    return window.initialLoad ? null :
+                        '/posts/tag/' + stateParams.tag + '?xhr=1';
                 }
             })
             .state('posts-for-month', {
                 url: '/posts/:year/:month',
                 templateUrl: function (stateParams) {
-                    return '/posts/' + stateParams.year + '/' + stateParams.month + '?xhr=1';
+                    return window.initialLoad ? null :
+                        '/posts/' + stateParams.year + '/' + stateParams.month + '?xhr=1';
                 }
             })
             .state('page-details', {
                 url: '/page/:slug',
                 templateUrl: function (stateParams) {
-                    return '/page/' + stateParams.slug + '?xhr=1';
+                    return window.initialLoad ? null :
+                        '/page/' + stateParams.slug + '?xhr=1';
                 }
             })
             .state('archive', {
@@ -62,17 +70,20 @@
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     }
 
-    var initialLoad = true;
     function run($rootScope, $timeout, $location, $window) {
+        // initialise google analytics
+        $window.ga && $window.ga('create', 'UA-30211492-1', 'auto');
+
         $rootScope.$on('$stateChangeSuccess', function () {
             // hide mobile nav
             $rootScope.showNav = false;
 
             // track pageview
-            $window.ga && $window.ga('send', 'pageview', $location.url());
+            var urlWithoutHash = $location.url().split('#')[0];
+            $window.ga && $window.ga('send', 'pageview', urlWithoutHash);
 
-            // jump to top of page
-            if (!initialLoad) {
+            // jump to top of page if not initial page load
+            if (!window.initialLoad) {
                 document.body.scrollTop = document.documentElement.scrollTop = 0;
             }
 
@@ -81,7 +92,7 @@
                 SyntaxHighlighter.highlight();
             });
 
-            initialLoad = false;
+            window.initialLoad = false;
         });
     }
 
